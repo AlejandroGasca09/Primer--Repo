febb8bd6e24bdc2aa6f66932b98eed451b8cb07c
AlejandroGasca09 <119777270+AlejandroGasca09@users.noreply.github.com>
2023-05-07 21:02:55 -0600

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Ventana ventana = new Ventana("Paneles");
+        //Ventana ventana = new Ventana("Layouts");
+        //VentanaGrid ventanaGrid = new VentanaGrid("GridLayout");
+        VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a Fahrenheit");
     }
 }
